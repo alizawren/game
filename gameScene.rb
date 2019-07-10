d0321de8658e4559f1612637c7bf9f6f554bd2e9
alizawren <alissaren98@gmail.com>
2019-07-09 22:30:10 -0700

    Further abstraction

---
+++ gameScene.rb
@@ -0,0 +1,48 @@
+require 'gosu'
+require_relative './player.rb'
+require_relative './scene.rb'
+
+class GameScene < Scene
+  attr_accessor :background_image
+  def load
+    @background_image = Gosu::Image.new("img/space.png", :tileable => true)
+
+    @player = Player.new
+    @player.goto(50, 50)
+
+    @enemies = []
+  end
+
+  def unload
+  end
+  
+  def update
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+        @player.go_left
+      end
+      if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+        @player.go_right
+      end
+      if Gosu.button_down? Gosu::KB_UP
+        @player.go_up
+      end
+      if Gosu.button_down? Gosu::KB_DOWN
+        @player.go_down
+      end
+
+      for enemy in @enemies do 
+        enemy.update(@player.x, @player.y, @player.vel_x, @player.vel_y)
+      end
+      @player.move
+  end
+  
+  def draw
+    @background_image.draw(0, 0, 0)
+    @player.draw
+    for enemy in @enemies do 
+      enemy.draw
+    end
+  end
+end
+
+# Main.new.show
--- scene1.rb
+++ scene1.rb
@@ -1,9 +1,9 @@
 require 'gosu'
 require_relative './player.rb'
-require_relative './scene.rb'
+require_relative './gameScene.rb'
 require_relative './enemy.rb'
 
-class Scene1 < Scene
+class Scene1 < GameScene
   attr_accessor :background_image
   def load
     @background_image = Gosu::Image.new("img/space.png", :tileable => true)
@@ -20,33 +20,6 @@ class Scene1 < Scene
   def unload
   end
   
-  def update
-    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
-        @player.go_left
-      end
-      if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
-        @player.go_right
-      end
-      if Gosu.button_down? Gosu::KB_UP
-        @player.go_up
-      end
-      if Gosu.button_down? Gosu::KB_DOWN
-        @player.go_down
-      end
-
-      for enemy in @enemies do 
-        enemy.update(@player.x, @player.y, @player.vel_x, @player.vel_y)
-      end
-      @player.move
-  end
-  
-  def draw
-    @background_image.draw(0, 0, 0)
-    @player.draw
-    for enemy in @enemies do 
-      enemy.draw
-    end
-  end
 end
 
 # Main.new.show
